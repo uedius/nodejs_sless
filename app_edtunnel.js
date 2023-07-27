873f51e625460aeee6a6fe525587d2db5b1fdbfc
uedius <43091502+uedius@users.noreply.github.com>
2023-07-27 22:23:28 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '9e5cba27-1ee2-44d1-b5bb-2614aa733123';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
